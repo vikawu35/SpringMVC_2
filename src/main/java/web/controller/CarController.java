51345f3f08a7b4669c37dc241ce3dc1575686431
vikawu35 <122668662+vikawu35@users.noreply.github.com>
2023-08-14 21:23:27 +0900

    Update CarController.java

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,33 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
-    private List<Car> carList;
+
+    @Autowired
     private CarService carService;
-
-    public CarController() {
-        carList = new ArrayList<>();
-        carList.add(new Car("White", 2020,"Toyota"));
-        carList.add(new Car("Black", 2019, "Honda"));
-        carList.add(new Car("Red", 2022,"Ford"));
-        carList.add(new Car("Black", 2021, "Chevrolet"));
-        carList.add(new Car("White", 2023, "BMW"));
-    }
-
+    
+    
     @GetMapping("/cars")
-    public String getCars(Model model) {
-        List<Car> cars = carService.getCars(carList.size(), carList);
-        model.addAttribute("cars", cars);
-        return "cars";
-    }
-    @GetMapping(value = "/cars", params = "count")
-    public String getCarsWithCount(Model model, @RequestParam("count") int count) {
-        List<Car> cars = carService.getCars(count, carList);
+    public String getCars(@RequestParam(name = "count", required = false, defaultValue = "0") int count, Model model) {
+        List<Car> cars = (count <= 0 || count >= 5) ? carService.getAllCars() : carService.getCars(count);
         model.addAttribute("cars", cars);
         return "cars";
     }
 }
+
