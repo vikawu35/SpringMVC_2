d201a8870f0174ec48db2d19dbd307d87dfd5446
vikawu35 <122668662+vikawu35@users.noreply.github.com>
2023-08-14 21:25:27 +0900

    Update CarService.java

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -4,15 +4,26 @@ package web.service;
 import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CarService {
-    public List<Car> getCars(int count, List<Car> carList) {
-        if (count >= carList.size() || count <= 0) {
-            return carList;
-        } else {
-            return carList.subList(0, count);
-        }
+    private List<Car> carList = new ArrayList<>();
+
+    public CarService() {
+        carList.add(new Car("White", 2020,"Toyota"));
+        carList.add(new Car("Black", 2019, "Honda"));
+        carList.add(new Car("Red", 2022,"Ford"));
+        carList.add(new Car("Black", 2021, "Chevrolet"));
+        carList.add(new Car("White", 2023, "BMW"));
+    }
+
+    public List<Car> getAllCars() {
+        return carList;
+    }
+
+    public List<Car> getCars(int count) {
+        return carList.subList(0, Math.min(count, carList.size()));
     }
 }
